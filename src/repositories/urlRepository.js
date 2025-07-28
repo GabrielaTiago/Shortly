159dca4ab30514fa973b28a3d445c76259d082df
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-07-28 20:09:55 -0300

    refactor: Update URL repository functions and improve query structure

--- src/repositories/urlRepository.js
+++ src/repositories/urlRepository.js
@@ -1,27 +1,32 @@
-import { connection } from '../database/postgres';
+import { connection } from '../database/postgres.js';
 
 async function createShortUrl(shortUrl, url, userId) {
   await connection.query('INSERT INTO urls ("shortUrl", url, "userId") VALUES ($1, $2, $3)', [shortUrl, url, userId]);
 }
 
-async function getUrlsById(urlId) {
-  const res = await connection.query('SELECT id, "shortUrl", url FROM urls WHERE id = $1', [urlId]);
+async function getUrlById(urlId) {
+  const res = await connection.query('SELECT id, "shortUrl", url, "userId" FROM urls WHERE id = $1', [urlId]);
   return res;
 }
 
-async function redirectToShortUrl(shortUrl) {
+async function getUrlByShortUrl(shortUrl) {
   const res = await connection.query('SELECT * FROM urls WHERE "shortUrl" = $1', [shortUrl]);
   return res;
 }
 
-async function deleteShortUrl(shortUrl) {
-  await connection.query('DELETE FROM urls WHERE "shortUrl" = $1', [shortUrl]);
+async function updateVisitCount(shortUrl, visitCount) {
+  await connection.query('UPDATE urls SET "visitCount" = $1 WHERE "shortUrl" = $2', [visitCount, shortUrl]);
+}
+
+async function deleteShortUrl(urlId) {
+  await connection.query('DELETE FROM urls WHERE id = $1', [urlId]);
 }
 
 const urlRepository = {
   createShortUrl,
-  getUrlsById,
-  redirectToShortUrl,
+  getUrlById,
+  getUrlByShortUrl,
+  updateVisitCount,
   deleteShortUrl,
 };
 
