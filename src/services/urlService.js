a33eca9302222734d375457ead0dac07555ac699
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-07-28 20:10:22 -0300

    feat: Implement URL service with CRUD operations for short URLs

---
+++ src/services/urlService.js
@@ -0,0 +1,61 @@
+import { nanoid } from 'nanoid/non-secure';
+import urlRepository from '../repositories/urlRepository.js';
+import userService from './userService.js';
+
+async function createShortUrl(url, userId) {
+  await userService.checkUserExists(userId);
+  const shortUrl = generateShortUrl();
+
+  await urlRepository.createShortUrl(shortUrl, url, userId);
+  return { shortUrl };
+}
+
+async function getUrlById(urlId) {
+  let { rows: url, rowCount } = await urlRepository.getUrlById(urlId);
+    validateUrlExists(rowCount);
+    delete url[0].userId;
+    return url[0];
+}
+
+async function redirectToShortUrl(shortUrl) {
+  const { rows: url, rowCount } = await urlRepository.getUrlByShortUrl(shortUrl);
+  validateUrlExists(rowCount);
+  await updateVisitCount(shortUrl, url[0].visitCount);
+  return url[0].url;
+}
+
+async function updateVisitCount(shortUrl, visitCount) {
+  const updatedVisitCount = visitCount + 1;
+  await urlRepository.updateVisitCount(shortUrl, updatedVisitCount);
+}
+
+async function deleteShortUrl(urlId, userId) {
+  await userService.checkUserExists(userId);
+  const { rowCount, rows: url } = await urlRepository.getUrlById(urlId);
+  validateUrlExists(rowCount);
+  if (url[0].userId !== userId) {
+    const error = { type: 'unauthorized', message: 'User not authorized to delete this URL' };
+    throw error;
+  }
+  await urlRepository.deleteShortUrl(urlId);
+}
+
+function generateShortUrl() {
+  return nanoid(10);
+}
+
+function validateUrlExists(rowCount) {
+  if (rowCount === 0) {
+    const error = { type: 'not_found', message: 'URL not found' };
+    throw error;
+  }
+}
+
+const urlService = {
+  createShortUrl,
+  getUrlById,
+  redirectToShortUrl,
+  deleteShortUrl,
+};
+
+export default urlService;
