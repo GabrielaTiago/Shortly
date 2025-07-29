8c0d37722177e84337ce0687513c669a05d2f27e
GabrielaTiago <gabrielatiagodearaujo@outlook.com>
2025-07-29 17:12:48 -0300

    docs: Add Swagger documentation setup and configuration

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import authRouter from './authRouter.js';
 import rankingRouter from './rankingRouter.js';
+import swaggerUi from 'swagger-ui-express';
 import urlsRouter from './urlRouter.js';
 import userRouter from './usersRouter.js';
+import swaggerDocument from '../../swagger.json' with { type: 'json' };
 
 const router = Router();
 
+router.use('/documentation', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 router.use(authRouter);
 router.use('/urls', urlsRouter);
 router.use('/users', userRouter);
